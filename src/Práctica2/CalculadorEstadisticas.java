6abb3c5bb02c78f2ff264babfee9f7cff32e6aad
Ciro <Ciro@Ciro-PC>
2014-11-13 23:13:42 +0000

    13 nov

--- src/Práctica2/CalculadorEstadisticas.java
+++ src/Práctica2/CalculadorEstadisticas.java
@@ -1,2 +1,33 @@
 package Práctica2;
 
+public class CalculadorEstadisticas {
+
+    // modo: false = inverso, true = aleatorio
+    public static DatosEstadisticos estadisticaOrdenaVector(OrdenarVector metodo, int  tam_vector,
+        boolean modo) {
+        
+        DatosEstadisticos[] vectorTiempos = {new DatosEstadisticos(), new DatosEstadisticos()};
+        
+        
+        return vectorTiempos;
+    }
+    
+    public static DatosEstadisticos[][] estadisticasOrdenaVectores(OrdenarVector m, int[] tam_vectores) {
+        DatosEstadisticos[][] matrizTiempos = new DatosEstadisticos[tam_vectores.length][2];
+        
+        
+        return matrizTiempos;
+    }
+    
+    public MediaDatosEstadisticos estadisticaOrdenaVectorAleatorio(OrdenaVector metodo, int tam) {
+    
+    }
+    
+    public DatosEstadisticos[] estadisticasOrdenaVectoresInversos() {
+    
+    }
+    
+    public MediaDatosEstadisticos[] estadisticasOrdenaVectoresAleatorios() {
+    
+    }
+}
--- src/Práctica2/MediaDatosEstadisticos.java
+++ src/Práctica2/MediaDatosEstadisticos.java
@@ -1,2 +1,21 @@
 package Práctica2;
 
+public class MediaDatosEstadisticos {
+
+    public void añadeDatosEstadisticos(DatosEstadisticos datos) {
+    
+    }
+    
+    public float dameMediaTiempos() {
+        return (float) 1.5;
+        
+    }
+    
+    public float dameMediaComparaciones() {
+    
+    }
+    
+    public float dameMediaMovimientos() {
+    
+    }
+}
